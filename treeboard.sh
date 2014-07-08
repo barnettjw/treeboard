44b24f331601bf21ec0ecefaa772d0483a9da570
James Barnett <barnettjw@gmail.com>
2014-07-08 01:59:52 +0000

    Updated Treehouse Teacher's list

--- treeboard.sh
+++ treeboard.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 #edit these variables
-ducksboard_api="4Y5AoALWKKsH03KUg587VeyRMAIBLBpi0NMsMkCMlNecNQDuz3"
-ducksboard_widget_leaderboard="166564"
-ducksboard_widget_total="166294"
-ducksboard_widget_text="166531"
-treehouse_users=('jim' 'nickpettit' 'bendog24' 'pasanpr' 'chalkers' 'guil' 'amit' 'jasonseifer' 'mathelme' 'dangorgone' 'randyhoyt' 'zgordon' )
-
+#db = ducksboard
+db_api="4Y5AoALWKKsH03KUg587VeyRMAIBLBpi0NMsMkCMlNecNQDuz3"
+db_leaderboard="166564"
+db_total="166294"
+db_text="166531"
+treehouse_users=('nickpettit' 'amit' 'jasonseifer' 'bendog24' 'guil' 'chalkers' 'zgordon' 'hamptonpaulk' 'davemcfarland' 'kennethlove' 'joykesten2' 'pasan')
 ###################
 echo > temp
 echo > sorted
-
 let total=0
-#loop through users getting info via curl calls against json
 count=${#treehouse_users[@]}
 for (( i=0; i < count; i++)); do
 	curl -s http://teamtreehouse.com/${treehouse_users[${i}]}.json > json
@@ -40,12 +38,14 @@ done < sorted
 data="{\"value\": {\"board\": [$data]}}"
 total="{\"value\": $total}"
 #echo $data | python -mjson.tool #debug check for properly formatted json
-curl -s https://push.ducksboard.com/values/$ducksboard_widget_leaderboard -u $ducksboard_api:x -d"$data"
-curl -s https://push.ducksboard.com/values/$ducksboard_widget_total -u $ducksboard_api:x -d"$total"
+url="curl -s https://push.ducksboard.com/values"
+$url/$db_leaderboard -u $db_api:x -d"$data"
+$url/$db_total -u $db_api:x -d"$total"
 
 #get top user from sorted, remove quotes around named that were added by jq, use awk to parse csv
 leader=$(head -1 sorted | sed s/\"//g | awk -F ',' '{print "Current Leader is " $1 " with " $2 " points & " $3 " badges";}') 
 text="{\"timestamp\": $(date +%s),\"value\": {\"content\": \"$leader\"}}"
-curl -s https://push.ducksboard.com/values/$ducksboard_widget_text -u $ducksboard_api:x -d"$text"
+$url/$db_text -u $db_api:x -d"$text"
+
 date +"%D %R %Z" #add date to cronjob log
 echo
